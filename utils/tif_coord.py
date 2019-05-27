cb32a5f94a17a9b58ecf8fbe32cab1c4525f2dd9
Jan Mandel <jan.mandel@gmail.com>
2019-05-27 09:47:43 -0600

    adding inverse transform

--- utils/tif_coord.py
+++ utils/tif_coord.py
@@ -3,7 +3,22 @@
 
 import gdal,osr
 
-def xy2latlon(name,x,y):
+def crs_ref():
+        # create lat/long crs with WGS84 datum
+        # same as PROJ.4 : +proj=longlat +datum=WGS84 +no_defs
+        # test with gdalsrsinfo epsg:4326
+        crs = osr.SpatialReference()
+        #crs.ImportFromEPSG(4326) # 4326 is the EPSG id of lat/long crs 
+        crs.SetWellKnownGeogCS('WGS84')
+        return crs
+
+def crs_ds(ds):
+        # get CRS from dataset 
+        crs = osr.SpatialReference()
+        crs.ImportFromWkt(ds.GetProjectionRef())
+        return crs
+
+def xy2lonlat(name,x,y):
     try:
         ds=gdal.Open(name)
     except:
@@ -20,22 +35,41 @@ def xy2latlon(name,x,y):
         # shift to the center of the pixel
         posX += px_w / 2.0
         posY += px_h / 2.0
-    
-        # get CRS from dataset 
-        crs = osr.SpatialReference()
-        crs.ImportFromWkt(ds.GetProjectionRef())
-        # create lat/long crs with WGS84 datum
-        crsGeo = osr.SpatialReference()
-        crsGeo.ImportFromEPSG(4326) # 4326 is the EPSG id of lat/long crs 
-        # same as PROJ.4 : +proj=longlat +datum=WGS84 +no_defs
-        # test with gdalsrsinfo epsg:4326
-        t = osr.CoordinateTransformation(crs, crsGeo)
-        (lat, lon, z) = t.TransformPoint(posX, posY)
-        return lon, lat, z
 
+        t = osr.CoordinateTransformation(crs_ds(ds), crs_ref())
+        lat, lon, z = t.TransformPoint(posX, posY)
+        return lon, lat
+
+def lonlat2xy(name,lon,lat):
+    try:
+        ds=gdal.Open(name)
+    except:
+        print "gdal cannot open file %s" % name
+        raise
+    finally:
+        xoffset, px_w, rot1, yoffset, rot2, px_h = ds.GetGeoTransform()
+
+        t = osr.CoordinateTransformation(crs_ref(),crs_ds(ds))
+        posX, posY, z  = t.TransformPoint(lat,lon,0)
+
+        posX -= px_w / 2.0
+        posY -= px_h / 2.0
+
+        transform = ds.GetGeoTransform()
+        gt = [transform[0],transform[1],0,transform[3],0,transform[5]]
+        inverse_gt = gdal.InvGeoTransform(gt)
+  
+        x = inverse_gt[0] + inverse_gt[1] * posX + inverse_gt[2] * posY
+        y = inverse_gt[3] + inverse_gt[4] * posX + inverse_gt[5] * posY
+
+        return x,y
+    
 if __name__ == '__main__':
     name='ned_data.tif'
-    x=0
-    y=0
-    lon, lat, z = xy2latlon(name,x,y)
-    print lon,lat
+    x=100.5
+    y=777.3
+    lon, lat = xy2lonlat(name,x,y)
+    print x,y,lon,lat
+    xx,yy = lonlat2xy(name,lon,lat)
+    print xx,yy,lon,lat
+    print "error",xx-x,yy-y
