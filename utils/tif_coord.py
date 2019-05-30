0456752f8ef199cff2a6092a632e3669904a6841
Jan Mandel <jan.mandel@gmail.com>
2019-05-30 08:49:06 -0600

    adding computing distance to tif_coord.py

--- utils/tif_coord.py
+++ utils/tif_coord.py
@@ -1,7 +1,7 @@
 # compute coordinates of tif pixel
 # https://stackoverflow.com/questions/50191648/gis-geotiff-gdal-python-how-to-get-coordinates-from-pixel
 
-import gdal,osr,sys
+import gdal,osr,pyproj,sys
 
 def crs_ref():
         # create lat/long crs with WGS84 datum
@@ -55,18 +55,35 @@ def lonlat2xy(name,lon,lat):
         return x,y
 
 def test_inv(name,x,y):
+    print "converting to WGS84 lon lat and back"
     lon, lat = xy2lonlat(name,x,y)
-    print x,y,lon,lat
+    print 'pixel',x,y,'lon lat',lon,lat
     xx,yy = lonlat2xy(name,lon,lat)
-    print xx,yy,lon,lat
+    # print 'pixel',xx,yy,'lon lat',lon,lat
     print "error",xx-x,yy-y
     
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
+    if len(sys.argv) == 4:
+        name = sys.argv[1] 
+        x=float(sys.argv[2])
+        y=float(sys.argv[3])
+        test_inv(name,x,y)
+    if len(sys.argv) == 6:
+        name = sys.argv[1] 
+        x1=float(sys.argv[2])
+        y1=float(sys.argv[3])
+        x2=float(sys.argv[4])
+        y2=float(sys.argv[5])
+        test_inv(name,x1,y1)
+        test_inv(name,x2,y2)
+        lon1, lat1 = xy2lonlat(name,x1,y1)
+        lon2, lat2 = xy2lonlat(name,x2,y2)
+        geod = pyproj.Geod(ellps='WGS84')
+        azimuth1, azimuth2, distance = geod.inv(lon1, lat1, lon2, lat2)
+        print 'distance', distance
+        print 'azimuth', azimuth1, azimuth2
+    else:
         print "usage: file.tif x y"
+        print "usage: file.tif x1 y1 x2 y2"
         sys.exit(1)
-    name = sys.argv[1] 
-    x=float(sys.argv[2])
-    y=float(sys.argv[3])
-    test_inv(name,x,y)
 
