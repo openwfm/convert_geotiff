887feb99d59ab6926fa7514c289e8452eb7e9d8c
Jan Mandel <jan.mandel@gmail.com>
2019-06-07 23:08:35 -0600

    debugging

--- utils/convert_geotiff.py
+++ utils/convert_geotiff.py
@@ -35,14 +35,17 @@ def pos2pix(ds,posX,posY):
     x, y = gdal.ApplyGeoTransform(inverse_gt, posX, posY)
     return x,y
 
-def wrf2pix(ds,wrf_proj,wrf_posX,wrf_posY):
-    tif_proj = get_tif_proj(ds)
+def wrf2pix(ds,wrf_proj,tif_proj,wrf_posX,wrf_posY):
     tif_posX, tif_posY = pyproj.transform(wrf_proj,tif_proj,wrf_posX,wrf_posY)
     x, y = pos2pix(ds,tif_posX,wrf_posY) 
-    
+    print 'wrf_pos',wrf_posX,wrf_posY,'at pixel',x, y 
+    return x, y
 
-def get_bbox(name,sizex, sizey, lon_0, lat_0, lat_1, lat_2)
-    gs = gdal.Open(name)
+def get_bbox(name,sizex, sizey, lon_0, lat_0, lat_1, lat_2):
+    print 'getiff file ',name
+    print 'cutout size',sizex,sizey,'m','center lon lat',lon_0, lat_0
+    print 'truelats',lat_1, lat_2
+    ds = gdal.Open(name)
     radius = 6370e3
     wrf_proj = pyproj.Proj(proj='lcc',
             lat_1=lat_0,
@@ -50,47 +53,39 @@ def get_bbox(name,sizex, sizey, lon_0, lat_0, lat_1, lat_2)
             lat_0=lat_0,
             lon_0=lon_0,
             a=radius, b=radius, towgs84='0,0,0', no_defs=True)
+    print 'wrf_proj=',wrf_proj.srs
     tif_proj = get_tif_proj(ds)
+    print 'tif_proj=',tif_proj.srs
     ref_proj = pyproj.Proj(proj='lonlat',datum='WGS84',no_defs=True)
+    print 'ref_proj=',ref_proj.srs
     # given midpoint to WRF coordinates
-    ctrX_wrf, ctrY_wrf = pyproj.transform(ref_proj,wrf_proj,lon_0,lat_0)
+    wrf_ctrX, wrf_ctrY = pyproj.transform(ref_proj,wrf_proj,lon_0,lat_0)
     # corners 
-    tif_posX_ll, tif_posY_ll = pyproj.transform(wrf_proj,tif_proj,wrf_posX-sizex/2,wrf_posY-sizey/2)
-    tif_posX_ul, tif_posY_ul = pyproj.transform(wrf_proj,tif_proj,wrf_posX-sizex/2,wrf_posY+sizey/2)
-    tif_posX_lr, tif_posY_lr = pyproj.transform(wrf_proj,tif_proj,wrf_posX+sizex/2,wrf_posY-sizey/2)
-    tif_posX_ur, tif_posY_ur = pyproj.transform(wrf_proj,tif_proj,wrf_posX+sizex/2,wrf_posY+sizey/2)
-    tif_posX_min = min([tif_posX_ll,tif_posX_ul,tif_posX_lr,tif_posX_ur])
-    tif_posX_max = max([tif_posX_ll,tif_posX_ul,tif_posX_lr,tif_posX_ur])
-    
-
+    x_ll, y_ll = wrf2pix(ds,wrf_proj,tif_proj,wrf_ctrX-sizex/2,wrf_ctrY-sizey/2)
+    x_ul, y_ul = wrf2pix(ds,wrf_proj,tif_proj,wrf_ctrX-sizex/2,wrf_ctrY+sizey/2)
+    x_lr, y_lr = wrf2pix(ds,wrf_proj,tif_proj,wrf_ctrX+sizex/2,wrf_ctrY-sizey/2)
+    x_ur, y_ur = wrf2pix(ds,wrf_proj,tif_proj,wrf_ctrX+sizex/2,wrf_ctrY+sizey/2)
+    x_min = min([x_ll,x_ul,x_lr,x_ur])
+    x_max = max([x_ll,x_ul,x_lr,x_ur])
+    y_min = min([y_ll,y_ul,y_lr,y_ur])
+    y_max = max([y_ll,y_ul,y_lr,y_ur])
+    print 'bounding box x',x_min,x_max,'y',y_min,y_max
+    return x_min,x_max,y_min,y_max
+ 
 if __name__ == '__main__':
-    if len(sys.argv) in (4,6):
+    if len(sys.argv) in (6,8):
         name = sys.argv[1] 
-        xsize= sys.argv[2]
-        ysize= sys.argv[3]
-        lon_0= sys.argv[4]
-        lat_0= sys.argv[5]
-    else 
-        usage: python convert_geotif.py xsize ysize lon_0 lat_0 lat_1 lat_2
+        sizex= float(sys.argv[2])
+        sizey= float(sys.argv[3])
+        lon_0= float(sys.argv[4])
+        lat_0= float(sys.argv[5])
+    else:
+        print 'usage: python convert_geotif.py file_tif sizex sizey lon_0 lat_0 [lat_1 lat_2]'
         sys.exit(1)
-    if len(sys.argv) in (6,):
-        lat_1= sys.argv[6]
-        lat_2= sys.argv[7]
-    else
-        lat_1, lat_2 = lat_0, lat_0
-
-
-        print 'grid distance',xydist(name,x1,y1,x2,y2)
-        lon1, lat1 = xy2lonlat(name,x1,y1)
-        lon2, lat2 = xy2lonlat(name,x2,y2)
-        geod = pyproj.Geod(ellps='WGS84')
-        azimuth1, azimuth2, distance = geod.inv(lon1, lat1, lon2, lat2)
-        print 'geod distance', distance
-        print 'azimuth', azimuth1, azimuth2
     if len(sys.argv) in (8,):
-        lon_0=float(sys.argv[6])
-        lat_0=float(sys.argv[7])
-        print 'reference lon %s lat %s' % (lon_0,lat_0)
-        lccd = lccdist(name,lon_0,lat_0,x1,y1,x2,y2)
-        print 'LCC distance ',lccd
+        lat_1= float(sys.argv[6])
+        lat_2= float(sys.argv[7])
+    else:
+        lat_1, lat_2 = lat_0, lat_0
+    get_bbox(name,sizex, sizey, lon_0, lat_0, lat_1, lat_2)
 
